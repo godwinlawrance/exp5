aa642ebbfdc15a92385fc3de6b488d4519ee609b
Godwin Lawrance L <godilawra@gmail.com>
2025-12-30 19:26:27 +0530

    success

---
+++ ServerProject/ServerApp/views.py
@@ -0,0 +1,32 @@
+
+from django.shortcuts import render
+
+def power_calculator(request):
+    power = None 
+    intensity = None
+    resistance = None 
+
+    if request.method == 'POST':
+        
+        
+        intensity = request.POST.get('intensity','0')
+        resistance = request.POST.get('resistance','0')
+
+        
+        if intensity and resistance:
+            try:
+            
+                I = float(intensity)
+                R = float(resistance)
+                power = I**2 * R
+                print('request=',request)
+                print('intensity=',I)
+                print('resistance=',R)
+                print('power=',power)  
+
+            except ValueError:
+                power = "Invalid input. Please enter numerical values."
+
+    
+    return render(request, 'Math.html', {'power': power, 'intensity': intensity, 'resistance': resistance})
+# Create your views here.
